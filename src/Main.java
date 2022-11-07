f7eb1edb10e85a40728cacd4c3f8302779f7216f
pepprond <rondelli95@gmail.com>
2022-11-07 17:04:08 +0100

    final commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal("lion");
+
+        animal1.animalSound();
+        animal1.animalSound("high");
+        animal1.animalSound("low");
+        animal1.animalSound("ciao");
+
+
+    }
+}
